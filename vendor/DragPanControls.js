c87407737100f079aa795cb7318609d0237629fe
Jerome Etienne <jerome.etienne@gmail.com>
2011-12-17 19:05:05 +0100

    renamed  DragPanControls.js in three.js namespace

--- vendor/DragPanControls.js
+++ vendor/DragPanControls.js
@@ -1,17 +1,16 @@
-var THREEx		= THREEx 		|| {};
-
-THREEx.DragPanControl	= function(object, domElement)
+THREE.DragPanControls	= function(object, domElement)
 {
 	this._object	= object;
 	this._domElement= domElement || document;
 
-	// parameter that you can change after initialisation
+	// parameters that you can change after initialisation
 	this.target	= new THREE.Vector3(0, 0, 0);
 	this.speedX	= 0.03;
 	this.speedY	= 0.03;
 	this.rangeX	= -40;
 	this.rangeY	= +40;
 
+	// private variables
 	this._mouseX	= 0;
 	this._mouseY	= 0;
 
@@ -25,27 +24,27 @@ THREEx.DragPanControl	= function(object, domElement)
 	this._domElement.addEventListener( 'touchmove', this._$onTouchMove, false );
 }
 
-THREEx.DragPanControl.prototype.destroy	= function()
+THREE.DragPanControls.prototype.destroy	= function()
 {
 	this._domElement.removeEventListener( 'mousemove', this._$onMouseMove, false );
 	this._domElement.removeEventListener( 'touchstart', this._$onTouchStart,false );
 	this._domElement.removeEventListener( 'touchmove', this._$onTouchMove, false );
 }
 
-THREEx.DragPanControl.prototype.update	= function(event)
+THREE.DragPanControls.prototype.update	= function(event)
 {
 	this._object.position.x += ( this._mouseX * this.rangeX - this._object.position.x ) * this.speedX;
 	this._object.position.y += ( this._mouseY * this.rangeY - this._object.position.y ) * this.speedY;
 	this._object.lookAt( this.target );
 }
 
-THREEx.DragPanControl.prototype._onMouseMove	= function(event)
+THREE.DragPanControls.prototype._onMouseMove	= function(event)
 {
 	this._mouseX	= ( event.clientX / window.innerWidth ) - 0.5;
 	this._mouseY	= ( event.clientY / window.innerHeight) - 0.5;
 }
 
-THREEx.DragPanControl.prototype._onTouchStart	= function(event)
+THREE.DragPanControls.prototype._onTouchStart	= function(event)
 {
 	if( event.touches.length != 1 )	return;
 
@@ -55,7 +54,7 @@ THREEx.DragPanControl.prototype._onTouchStart	= function(event)
 	this._mouseY	= ( event.touches[ 0 ].pageY / window.innerHeight) - 0.5;
 }
 
-THREEx.DragPanControl.prototype._onTouchMove	= function(event)
+THREE.DragPanControls.prototype._onTouchMove	= function(event)
 {
 	if( event.touches.length != 1 )	return;
 
